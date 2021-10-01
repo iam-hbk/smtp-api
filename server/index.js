baa9bb8dd40193656d993880a0322130347204aa
Heritier Kaumbu <70110128+iam-hbk@users.noreply.github.com>
2021-10-01 17:39:38 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,5 +2,30 @@ const express =require("express");
 const PORT = process.env.PORT || 3001;
 const app =express();
 
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+        host: 'smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+            user: "1aacc138aae93e",
+    pass: "615e75d714de85"
+        }
+})
+message = {
+    from: "from-example@email.com",
+    to: "to-example@email.com",
+    subject: "Subject",
+    text: "Hello SMTP Email"
+}
+
+transporter.sendMail(message, function(err, info) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(info);
+    }
+
+
 // app.get("/api",(req,res)=>{res.json({message:"Hello from server!"})});
 app.listen(PORT,()=>{console.log(`Server is running on ${PORT}`)})
