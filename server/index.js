3054df9a9f4f874714942c4e00d41cb51defd1d9
Heritier Kaumbu <70110128+iam-hbk@users.noreply.github.com>
2021-10-01 17:35:30 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express =require("express");
 const PORT = process.env.PORT || 3001;
 const app =express();
 
-app.get("/api",(req,res)=>{res.json({message:"Hello from server!"})});
+// app.get("/api",(req,res)=>{res.json({message:"Hello from server!"})});
 app.listen(PORT,()=>{console.log(`Server is running on ${PORT}`)})
